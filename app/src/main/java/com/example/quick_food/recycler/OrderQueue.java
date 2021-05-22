af2d208f403ea16460c08969ddf467f02124c639
0772412113 <0772412113>
2021-05-22 23:40:08 +0530

    profile edit

--- app/src/main/java/com/example/quick_food/recycler/OrderQueue.java
+++ app/src/main/java/com/example/quick_food/recycler/OrderQueue.java
@@ -1,19 +1,25 @@
 package com.example.quick_food.recycler;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.quick_food.Adapters.OrderQueueAdapter;
 import com.example.quick_food.GetterSetters.OrderDetails;
+import com.example.quick_food.QRCodeScanner;
 import com.example.quick_food.R;
+import com.example.quick_food.UserProfile;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -38,12 +44,18 @@ public class OrderQueue extends AppCompatActivity {
     String isVendorLogged;
     String currentUserId;
     boolean isOngingOrders;
+    private boolean userIsVender = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_queue);
         getSupportActionBar().setTitle("Orders - Ongoing");
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
         orderHistory = (Button) findViewById(R.id.order_history);
         ongoinOrders = (Button) findViewById(R.id.ongoing_orders);
@@ -165,6 +177,59 @@ public class OrderQueue extends AppCompatActivity {
                         }
                     }
                 });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        final SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        String theVender = prefs.getString("userIsVender", "");
+
+
+        MenuItem QRScanner = menu.findItem(R.id.QR_scanner);
+        MenuItem Profile = menu.findItem(R.id.profile_menu);
+        MenuItem Cart = menu.findItem(R.id.cart_icon_menu);
+        MenuItem Order = menu.findItem(R.id.Orders);
+
+
+        if (!theVender.equals("")) {
+            userIsVender = true;
+
+            QRScanner.setVisible(true);
+            Profile.setVisible(false);
+            Cart.setVisible(false);
+            Order.setVisible(false);
+        } else {
+
+            QRScanner.setVisible(false);
+            Profile.setVisible(false);
+            Cart.setVisible(false);
+            Order.setVisible(false);
+
+        }
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.QR_scanner) {
+            Intent intent = new Intent(OrderQueue.this, QRCodeScanner.class);
+            startActivity(intent);
+            return true;
+        }
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
